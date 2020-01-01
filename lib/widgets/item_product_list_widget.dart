6f0eea798004454b005ca0f8209afd21feb86061
XinyueZ <dev.xinyue.zhao@gmail.com>
2020-01-01 16:11:41 +0800

    flutter-fashion-collective: adjust spacing on item

--- lib/widgets/item_product_list_widget.dart
+++ lib/widgets/item_product_list_widget.dart
@@ -16,70 +16,74 @@ class _ItemProductListWidgetState extends State<ItemProductListWidget> {
   Widget build(BuildContext context) {
     return Container(
       child: Card(
-        child: Row(
-          children: <Widget>[
-            Image(
-              width: 74.16,
-              height: 120,
-              image: NetworkImage(widget.product.thumbnail.toString()),
-            ),
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    widget.product.brandedName,
-                    maxLines: 2,
-                    style: Theme.of(context).textTheme.title,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10, bottom: 10),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        widget.product.seeMoreLabel,
-                        maxLines: 4,
-                        style: Theme.of(context).textTheme.subtitle,
-                        overflow: TextOverflow.ellipsis,
-                      ),
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              Image(
+                width: 74.16,
+                height: 120,
+                image: NetworkImage(widget.product.thumbnail.toString()),
+              ),
+              SizedBox(width: 15,),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      widget.product.brandedName,
+                      maxLines: 2,
+                      style: Theme.of(context).textTheme.title,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Align(
+                    Container(
+                      margin: EdgeInsets.only(top: 10, bottom: 10),
+                      child: Align(
                         alignment: Alignment.centerLeft,
                         child: Text(
-                          widget.product.priceLabel,
-                          maxLines: 1,
-                          style: Theme.of(context).textTheme.display1,
+                          widget.product.seeMoreLabel,
+                          maxLines: 4,
+                          style: Theme.of(context).textTheme.subtitle,
                           overflow: TextOverflow.ellipsis,
                         ),
                       ),
-                      Row(
-                        children: <Widget>[
-                          widget.product.inStock
-                              ? Image.asset(
-                            'assets/icons/ic_in_stock.png',
-                            width: 25,
-                            height: 25,
-                          )
-                              : Container(),
-                          widget.product.isPromotionalDeal
-                              ? Image.asset(
-                            'assets/icons/ic_promotional.png',
-                            width: 25,
-                            height: 25,
-                          )
-                              : Container(),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            widget.product.priceLabel,
+                            maxLines: 1,
+                            style: Theme.of(context).textTheme.display1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            widget.product.inStock
+                                ? Image.asset(
+                              'assets/icons/ic_in_stock.png',
+                              width: 25,
+                              height: 25,
+                            )
+                                : Container(),
+                            widget.product.isPromotionalDeal
+                                ? Image.asset(
+                              'assets/icons/ic_promotional.png',
+                              width: 25,
+                              height: 25,
+                            )
+                                : Container(),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
